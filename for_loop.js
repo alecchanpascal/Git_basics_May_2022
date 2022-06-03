87eae99defaba1836ecc5a63815d515651fb0619
Alec Chan <alec.pascal@gmail.com>
2022-06-03 16:53:55 +0000

    added functionality to for_loop.js

--- for_loop.js
+++ for_loop.js
@@ -1,8 +1,7 @@
-// arr = [1,2,3]
-arr = [5,6,7]
+arr = [1,2,3,5,6,7];
 for (let element of arr){
-    console.log(element)
+    console.log(element);
+    element += element;
+    console.log(element);
 }
-
-//change 2
 
